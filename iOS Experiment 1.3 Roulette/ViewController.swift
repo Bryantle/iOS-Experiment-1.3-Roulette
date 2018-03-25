050bc4f17956cb7686b73f102c0585c09beba67a
Bryantle <bryantl@berkeley.edu>
2018-03-25 16:09:30 -0700

    Created randomize function and fixed nonlocal problem with randomizer attributes. Still needs button to change text after the game has completed.

--- iOS Experiment 1.3 Roulette/ViewController.swift
+++ iOS Experiment 1.3 Roulette/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let roulettes = [0, 0, 0, 0, 1, 1, 1, 1, 2]
+    
+    @IBOutlet weak var buttonText: UIButton!
+    @IBOutlet weak var yourGuess: UISwitch!
+    @IBAction func changeGuess(_ sender: Any) {
+        if yourGuess.isOn{
+            yourGuess.onTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            var theGuess = 0
+        }
+        else {
+            yourGuess.onTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            var theGuess = 1
+        }
+    }
+    @IBAction func spinner(_ sender: Any) {
+        let randomIndex = Int(arc4random_uniform(UInt32(roulettes.count)))
+        let randomItem = roulettes[randomIndex]
+        if randomItem == theGuess {
+            buttonText.setTitle("You Win", for: <#T##UIControlState#>)
+        }
+        else {
+            buttonText.setTitle("You Lose", for: <#T##UIControlState#>)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        yourGuess.setOn(true, animated: true)
+        yourGuess.onTintColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        var theGuess = 0
     }
 
     override func didReceiveMemoryWarning() {
